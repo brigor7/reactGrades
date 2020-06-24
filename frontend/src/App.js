84dcdb4045cc110f8a74ab9be65bca9807e52e7e
=Bruno Igor <=brigor7@gmail.com>
2020-06-24 10:36:28 -0300

    Implementation <GradesControl> component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import * as api from './api/apiService';
 import Spinner from './components/Spinner';
+import GradesControl from './components/GradesControl';
 
 export default function App() {
   const [allGrades, setAllGrades] = useState([]);
@@ -17,12 +18,22 @@ export default function App() {
     getGrades();
   }, []);
 
+  const handleDeleteGrade = () => {};
+  const handlePersistGrade = () => {};
+
   return (
     <div className="container">
       <h2 className="center">React Grades</h2>
       <h3 className="center">Controle de notas</h3>
-      {allGrades.length > 0 && <p>Notas disponiveis</p>}
       {allGrades.length === 0 && <Spinner />}
+
+      {allGrades.length > 0 && (
+        <GradesControl
+          grades={allGrades}
+          onDeleteGrade={handleDeleteGrade}
+          onPersistGrade={handlePersistGrade}
+        />
+      )}
     </div>
   );
 }
---
+++ frontend/src/components/GradesControl.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GradesControl(props) {
+  return (
+    <div>
+      <p className="center">Notas disponiveis</p>
+    </div>
+  );
+}
