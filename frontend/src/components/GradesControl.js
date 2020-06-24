18ad7ff1f2dfb1f2aaa290d3a7adb09dfba00adc
=Bruno Igor <=brigor7@gmail.com>
2020-06-24 12:49:38 -0300

    Create table grades

--- frontend/src/components/GradesControl.js
+++ frontend/src/components/GradesControl.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export default function GradesControl({ grades, onDelete, onPersist }) {
   console.log(onDelete);
@@ -6,7 +6,31 @@ export default function GradesControl({ grades, onDelete, onPersist }) {
   return (
     <div>
       <p className="center">Notas disponiveis</p>
-      <span>{JSON.stringify(grades)}</span>
+      <table className="striped">
+        <thead>
+          <th>Aluno</th>
+          <th>Disciplina</th>
+          <th>Avaliação</th>
+          <th>Nota</th>
+          <th>&nbsp;</th>
+          <th>&nbsp;</th>
+        </thead>
+        <tbody>
+          {grades.map((grade) => {
+            return (
+              <tr key={grade.id}>
+                <td>{grade.student}</td>
+                <td>{grade.subject}</td>
+                <td>{grade.type}</td>
+                <td>{grade.value}</td>
+                <td>&nbsp;</td>
+                <td>{grade.isDeleted}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot></tfoot>
+      </table>
     </div>
   );
 }
