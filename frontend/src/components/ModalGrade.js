dc26f493c17fce8944f9837a24179609c19d3fd2
=Bruno Igor <=brigor7@gmail.com>
2020-06-24 22:31:41 -0300

    Implementação de modalGrade const e useEffect

--- frontend/src/components/ModalGrade.js
+++ frontend/src/components/ModalGrade.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
+
+import * as api from '../api/apiService';
 
 Modal.setAppElement('#root');
 
 export default function ModalGrade({ onSave, onClose, selectedGrade }) {
+  const [gradeValue, setGradeValue] = useState(selectedGrade.value);
+  const [gradeValidation, setGradeValidation] = useState({});
+  const [erroMessage, setErroMessage] = useState('');
+
+  useEffect(() => {
+    const validation = api.getValidationFromGradeType(selectedGrade.type);
+    setGradeValidation(validation);
+  }, [selectedGrade.type]);
+
   return (
     <div>
       <Modal isOpen={true} />
