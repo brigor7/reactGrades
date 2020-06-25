5f2bd7f2a3389868a5b2e768f9ed36583608b3dd
=Bruno Igor <=brigor7@gmail.com>
2020-06-24 22:15:33 -0300

    Implementação de janela modal

--- frontend/src/components/ModalGrade.js
+++ frontend/src/components/ModalGrade.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export default function ModalGrade({ onSave, onClose, selectedGrade }) {
-  return <div>ModalGrade</div>;
+  return (
+    <div>
+      <Modal isOpen={true} />
+    </div>
+  );
 }
