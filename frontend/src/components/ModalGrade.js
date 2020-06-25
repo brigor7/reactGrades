16fb910810f66666f6c882dfb4e72c736bd3c264
=Bruno Igor <=brigor7@gmail.com>
2020-06-24 22:37:54 -0300

    Validação de nota em gradeValue min e max

--- frontend/src/components/ModalGrade.js
+++ frontend/src/components/ModalGrade.js
@@ -8,12 +8,24 @@ Modal.setAppElement('#root');
 export default function ModalGrade({ onSave, onClose, selectedGrade }) {
   const [gradeValue, setGradeValue] = useState(selectedGrade.value);
   const [gradeValidation, setGradeValidation] = useState({});
-  const [erroMessage, setErroMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const validation = api.getValidationFromGradeType(selectedGrade.type);
     setGradeValidation(validation);
   }, [selectedGrade.type]);
+
+  useEffect(() => {
+    const { minValue, maxValue } = gradeValidation;
+
+    if (gradeValue < minValue || gradeValue > maxValue) {
+      setErrorMessage(
+        `O valor da nota deve ser entre ${minValue} e ${maxValue} (inclusive)`
+      );
+      return;
+    }
+    setErrorMessage('');
+  }, [gradeValue, gradeValidation]);
 
   return (
     <div>
